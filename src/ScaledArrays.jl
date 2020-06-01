3b75fce7de1392a4626a21e5e167fab0ae27a8a7
milankl <milankloewer@gmx.de>
2020-06-02 00:05:29 +0100

    with binary tree search

--- src/ScaledArrays.jl
+++ src/ScaledArrays.jl
@@ -1,6 +1,9 @@
 module ScaledArrays
 
-    export ScaledArray, unscale, rescale
+    export ScaledArray, unscale, rescale,
+            LinQuant16Array, LinQuant24Array
 
     include("scaledarray.jl")
+    include("binarytree.jl")
+    include("quantarray.jl")
 end
